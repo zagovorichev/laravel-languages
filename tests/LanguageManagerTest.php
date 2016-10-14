55ff04bf0c8ab1fae5d858783c44b42b75a953fb
Alexander Zagovorichev <zagovorichev@gmail.com>
2016-10-14 17:10:32 +0300

    fixed composer

---
+++ tests/LanguageManagerTest.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MIT Public License for more details.
+ *
+ * Copyright (c) 2016. (original work) Blog-Tree.com;
+ *
+ * @author A.Zagovorichev <zagovorichev@gmail.com>
+ */
+
+namespace Zagovorichev\Laravel\Languages\tests;
+
+
+class LanguageManagerTest extends \PHPUnit_Framework_TestCase
+{
+
+}
