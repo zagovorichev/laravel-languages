8fd953906cf10f6ebdfa382c3e71599349bc714e
zagovorichev <zagovorichev@gmail.com>
2016-10-20 22:34:39 +0300

    fixed path manager

--- src/Manager/PathManager.php
+++ src/Manager/PathManager.php
@@ -35,8 +35,9 @@ class PathManager extends Manager
     public function get()
     {
         $lang = false;
-        if (preg_match($this->getConfig()->get('pathRegEx', ''), $this->request->path(), $match) !== false) {
-            $lang = $match[1];
+        $regEx = $this->getConfig()->get('pathRegExp', '');
+        if ($d = preg_match('|'. $regEx . '|ui', $this->request->path(), $match) !== false) {
+            $lang = $this->detectLang($match[1]);
         }
 
         return $lang;
@@ -55,12 +56,6 @@ class PathManager extends Manager
 
     public function has()
     {
-        $lang = false;
-        $regEx = $this->getConfig()->get('pathRegExp', '');
-        if ($d = preg_match('|'. $regEx . '|ui', $this->request->path(), $match) !== false) {
-            $lang = $this->detectLang($match[1]);
-        }
-
-        return !!$lang;
+        return !!$this->get();
     }
 }
